5ce7fec7711f6d5a729348d5b3dd23a3c7e400a2
ChamathJ <ranasingherpcgj.21@uom.lk>
2024-02-23 19:10:46 +0530

    Included a api test I did with Flask (Just in case)

---
+++ api-test/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import json
+
+app = Flask(__name__)
+
+@app.get('/api')
+def apiReturn():
+    data = {'name':'Chamath', 'age':30}
+    dataJson = json.dumps(data)
+    return dataJson
+
+app.run(debug=True)
