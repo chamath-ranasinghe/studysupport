dc1b62321f8a1ae307158c85e483214f72d0f2bd
SabAneesah <aneesha13sabar@gmail.com>
2024-04-13 23:03:08 +0530

    Card Component Created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Navbar from "./Components/NavBar";
+import Card from "./Components/Card";
 
 
 function App() {
   return (
     <div className="App">
     <Navbar/>  
+    <Card/>
     </div>
   );
 }
---
+++ src/Components/Card.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Card = () => {
+  return (
+    <div className="w-1/4 bg-purple-600 shadow-lg rounded-lg p-6 m-2">
+      <h2 className="text-white text-2xl font-bold mb-4">Card Title</h2>
+      <p className="text-white">This is the content of the card.</p>
+    </div>
+  );
+};
+
+export default Card;
