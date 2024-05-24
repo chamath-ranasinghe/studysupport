7536bb4d6700a2c0f6768b73ceab05e5f12d8263
ChamathJ <ranasingherpcgj.21@uom.lk>
2024-05-25 02:09:26 +0530

    Started working on Manual Flashcard Creation UI

--- src/Components/FlashCardTile.js
+++ src/Components/FlashCardTile.js
@@ -1,18 +1,32 @@
 import React from 'react';
-
-import CrossImg from "../Assets/Images/cross-small.png";
+import { XMarkIcon } from '@heroicons/react/24/solid';
 
 const FlashCardTile = (props) => {
   return (
-    <div className="bg-white shadow-lg  mt-16 mx-10 rounded-lg p-4 h-28"> {/* Increased height */}
-        <div className='grid grid-cols-10 grid-flow-col'>
-            <div className='col-span-1 mt-5'>
-            {/*Number */}
-            <h5 className='text-md font-bold text-black'>{props.number}</h5></div>
-            <div className='col-span-5 bg-gray-300 border-2 border-b-black shadow-md rounded-lg p-4  mb-5 mt-3 mx-8 '>Enter Term</div>
-            <div className='col-span-5 bg-gray-300 border-2 border-b-black shadow-md rounded-lg p-4  mb-5 mt-3  '>Enter Definition</div>
-            <div className='col-span-1 bg-gray-300 border-2 border-b-black shadow-md rounded-lg p-4  mb-5 mt-3  '></div>
+    <div className="bg-white shadow-lg mt-16 mx-10 rounded-lg p-4 h-28"> {/* Increased height */}
+      <div className='grid grid-cols-10 grid-flow-col'>
+        <div className='col-span-1 mt-5'>
+          {/* Number */}
+          <h5 className='text-md font-bold text-black'>{props.number}</h5>
         </div>
+        <input
+          type="text"
+          placeholder="Enter Term"
+          className='col-span-5 bg-gray-300 border-2 border-b-black shadow-md rounded-lg p-4 mb-5 mt-3 mx-8'
+          value={props.term}
+          onChange={props.sendTermToParent}
+        />
+        <input
+          type="text"
+          placeholder="Enter Definition"
+          className='col-span-5 bg-gray-300 border-2 border-b-black shadow-md rounded-lg p-4 mb-5 mt-3'
+          value={props.definition}
+          onChange={props.sendDefinitionToParent}
+        />
+        <button className='col-span-1 bg-red-500 hover:bg-red-400 border-2 border-b-black shadow-md rounded-lg p-4 mb-5 mt-3 flex justify-center items-center' onClick={props.removeCard}>
+          <XMarkIcon className="h-6 w-6 text-white" />
+        </button>
+      </div>
     </div>
   );
 };
--- src/Pages/FlashCardPage.js
+++ src/Pages/FlashCardPage.js
@@ -1,23 +1,50 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import FlashCardInput from "../Components/FlashCardInput";
 import FlashCardTile from "../Components/FlashCardTile";
-import FlashCardTileContainer from "../Components/FlashCardTilesContainer";
 import Navbar from "../Components/NavBar";
-import { DataContext } from '../routes/UserRoute';
+import { DataContext } from "../routes/UserRoute";
 
 function FlashCardPage() {
-    const data = useContext(DataContext);
-    return (
-        <div>
-            <Navbar/> 
-            <FlashCardInput items={data} />
-            <FlashCardTile number={"1"}/>
-            <FlashCardTile number={"2"}/>
-            <FlashCardTile number={"3"}/>
-            <FlashCardTileContainer/>
-         
-        </div>
-    );
+  const data = useContext(DataContext);
+  const [flashcards, setFlashcards] = useState([]);
+  const [term, setTerm] = useState(null);
+  const [definition , setDefinition] = useState(null);
+
+  const addFlashcard = ()=>{
+    setFlashcards(flashcards => [...flashcards, {term: term, definition: definition}]);
+    setTerm(null);
+    setDefinition(null);
+
   }
-  
-  export default FlashCardPage;
+
+  const handleTerm = (e)=>{
+    setTerm(e.target.value);
+  }
+
+  const handleDefinition = (e)=>{
+    setDefinition(e.target.value);
+  }
+
+  const handleRemoveCard = (index) => {
+    setFlashcards(prevCards => prevCards.filter((_, i) => i !== index));
+  };
+
+  return (
+    <div>
+      <Navbar />
+      <FlashCardInput items={data} />
+
+      {flashcards.map((card,index)=>(
+        <FlashCardTile key={index} removeCard = {()=> handleRemoveCard(index)} sendTermToParent = {handleTerm} sendDefinitionToParent = {handleDefinition} term = {card.term} definition = {card.definition} number={index+1} />
+      ))}  
+
+      <button className="flex justify-center items-center bg-white shadow-lg my-10 mx-10 rounded-lg p-4 h-28" style={{width: "94vw"}} onClick={addFlashcard}>
+        <div className="text-xl font-bold text-purple-500 hover:text-purple-600 cursor-pointer">
+          + Add Card
+        </div>
+      </button>
+    </div>
+  );
+}
+
+export default FlashCardPage;
